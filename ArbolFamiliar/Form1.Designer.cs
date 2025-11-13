2a07ae4964d5622c8562a0826ceaed069b4c27d5
yoel4545 <yoelr086@gmail.com>
2025-11-12 18:11:22 -0600

    Boton de salida mas estetico

--- ArbolFamiliar/Form1.Designer.cs
+++ ArbolFamiliar/Form1.Designer.cs
@@ -94,7 +94,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Location = new System.Drawing.Point(12, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 32);
+            this.button1.Size = new System.Drawing.Size(175, 105);
             this.button1.TabIndex = 3;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
