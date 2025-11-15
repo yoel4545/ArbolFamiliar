733d4ea06e70340a3ea6b7036feb0cba16132f2e
yoel4545 <yoelr086@gmail.com>
2025-11-15 17:07:21 -0600

    Boton volver en ayuda

--- ArbolFamiliar/Ayuda.Designer.cs
+++ ArbolFamiliar/Ayuda.Designer.cs
@@ -39,9 +39,9 @@
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(-18, -1);
+            this.button1.Location = new System.Drawing.Point(-3, 1);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 39);
+            this.button1.Size = new System.Drawing.Size(133, 58);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
